32c8c93327e24f35499b5365387bb4b08f7cc1da
nxchuong <chuong.nguyen@axonactive.com>
2019-06-24 16:13:00 +0700

    handle selected header item

--- src/main/java/nihongo/IndividualProfileController.java
+++ src/main/java/nihongo/IndividualProfileController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import profile.HeadNavigator;
 import profile.HeaderService;
 
 @Controller
@@ -16,16 +17,19 @@ public class IndividualProfileController {
 	@GetMapping("/contact")
 	public String myContact(Model model) {
 		model.addAttribute(HeaderService.HEADER_ATTR, HeaderService.getNavigatoritems());
+		model.addAttribute(HeaderService.SELECTED_ITEM_ATTR, HeadNavigator.CONTACT);
 		return "contact";
 	}
 	@GetMapping("/work")
 	public String myWork(Model model) {
 		model.addAttribute(HeaderService.HEADER_ATTR, HeaderService.getNavigatoritems());
+		model.addAttribute(HeaderService.SELECTED_ITEM_ATTR, HeadNavigator.WORK);
 		return "work";
 	}
 	@GetMapping("/about")
 	public String aboutMe(Model model) {
 		model.addAttribute(HeaderService.HEADER_ATTR, HeaderService.getNavigatoritems());
+		model.addAttribute(HeaderService.SELECTED_ITEM_ATTR, HeadNavigator.ABOUT);
 		return "about";
 	}
 }
--- src/main/java/profile/HeaderService.java
+++ src/main/java/profile/HeaderService.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class HeaderService {
 	private final static List<HeadNavigator> navigatorItems;
 	public static final String HEADER_ATTR = "headerAttribute";
+	public static final String SELECTED_ITEM_ATTR = "selectedItemAttr";
 
 	static {
 		navigatorItems = Arrays.asList(HeadNavigator.values());

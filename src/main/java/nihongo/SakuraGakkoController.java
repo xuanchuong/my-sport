f40f4ce1b5a94a06a0f5b3706fa050b2941ea7ae
nxchuong <chuong.nguyen@axonactive.com>
2019-10-21 17:05:09 +0700

    dynamic sakura content

--- src/main/java/nihongo/SakuraGakkoController.java
+++ src/main/java/nihongo/SakuraGakkoController.java
@@ -1,5 +1,8 @@
 package nihongo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,11 @@ public class SakuraGakkoController {
 	public String sakuraGakkoPage(Model model) {
 		model.addAttribute(HeaderService.HEADER_ATTR, HeaderService.getNavigatoritems());
 		model.addAttribute(HeaderService.SELECTED_ITEM_ATTR, HeadNavigator.SAKURA_GAKKO);
+		List<String> bunbo = new ArrayList<>();
+		for (int i = 0; i < 20; i++) {
+			bunbo.add("" + i);
+		}
+		model.addAttribute("bunbo", bunbo);
 		return "sakuraGakko";
 	}
 }

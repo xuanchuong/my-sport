447f822404ec0b470ddcf391001c95948490ffaf
nxchuong <chuong.nguyen@axonactive.com>
2019-10-21 10:47:49 +0700

    revert the changes

--- src/test/java/nihongo/NihongoSpringApplicationTests.java
+++ src/test/java/nihongo/NihongoSpringApplicationTests.java
@@ -10,11 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class NihongoSpringApplicationTests {
 
 	@Test
-	public void contextLoads() {
-		int X = 10;
-		int Y = 20;
-		int result = 0;
-		resut += ((Y - X) % 2 == 0) ? 0 : 1;
-	}
+	public void contextLoads() {}
 
 }

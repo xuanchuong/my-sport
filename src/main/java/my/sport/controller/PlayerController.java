8b0f526fa9478ba4f344a8392cf01eddc1aacee2
nxchuong <chuong.nguyen@axonactive.com>
2019-11-12 08:53:49 +0700

    move rest methods -> PlayerRestController

--- src/main/java/my/sport/controller/PlayerController.java
+++ src/main/java/my/sport/controller/PlayerController.java
@@ -1,39 +1,17 @@
 package my.sport.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import my.sport.model.Player;
-import my.sport.service.PlayerService;
-
-@RestController
+@Controller
 @RequestMapping("/player")
 public class PlayerController {
 
-	@Autowired
-	private PlayerService playerService;
-	
-	@GetMapping
-	public List<Player> getAllPlayers() {
-		return playerService.getAllPlayers();
-	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public HttpStatus insertPersone(@RequestBody Player player) {
-		return playerService.add(player) ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST;
-	}
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public HttpStatus deletePlayer(@PathVariable Long id) {
-		playerService.deletePlayer(id);
-		return HttpStatus.NO_CONTENT;
+	@GetMapping("/detail")
+	public String detailPlayer(@RequestParam(value = "id", required = true) String id) {
+		System.out.println(id);
+		return "playerInfo";
 	}
 }

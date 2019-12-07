2220ee7a5d38d6baf5e43395c27b316dd395fc8b
xuanchuongdp <xuanchuongdp@gmail.com>
2019-12-07 12:04:46 +0700

    make application deployable on tomcat

--- src/main/java/my/sport/MySportSpringApplication.java
+++ src/main/java/my/sport/MySportSpringApplication.java
@@ -2,9 +2,16 @@ package my.sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MySportSpringApplication {
+public class MySportSpringApplication extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MySportSpringApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySportSpringApplication.class, args);

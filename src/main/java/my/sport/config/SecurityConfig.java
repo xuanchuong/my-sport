a15c0d4d6182fefa83475e1ef4627e8f8185a3b9
xuanchuongdp <xuanchuongdp@gmail.com>
2020-02-23 18:18:22 +0700

    update contact page

--- src/main/java/my/sport/config/SecurityConfig.java
+++ src/main/java/my/sport/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-				.antMatchers("/player/detail/**", "/match/**")
+				.antMatchers("/player/detail/**", "/match/**", "/contact")
 				.authenticated()
 				.and()
 					.formLogin().loginPage("/login").permitAll()
--- src/main/java/my/sport/controller/ContactController.java
+++ src/main/java/my/sport/controller/ContactController.java
@@ -1,15 +1,25 @@
 package my.sport.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import my.sport.model.Player;
+import my.sport.service.FootballMatchService;
+import my.sport.service.PlayerService;
+
 @Controller
 @RequestMapping(value = {"/contact"})
 public class ContactController {
+
+	@Autowired
+	private PlayerService playerService;
 	@GetMapping
 	public String myContact(Model model) {
+		Player currentPlayer = playerService.getSessionPlayer();
+		model.addAttribute("user", currentPlayer);
 		return "contact";
 	}
 }

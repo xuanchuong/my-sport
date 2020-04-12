d9c5ec8ce8f73ef73b353dee3b7070d0a1aab0f4
xuanchuongdp <xuanchuongdp@gmail.com>
2020-04-12 10:44:12 +0700

    [db-h2] run app on local with pg or h2 profile

---
+++ src/test/java/my/sport/MySportSpringApplicationLocal.java
@@ -0,0 +1,12 @@
+package my.sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySportSpringApplicationLocal{
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySportSpringApplication.class, args);
+	}
+}

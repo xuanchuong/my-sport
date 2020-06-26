c1d42786ea4ecc6b2a53e7479d576173ad9aaed5
chuong.nguyen <chuong.nguyen@mobi.ch>
2020-06-26 16:27:58 +0700

    [remove-unused-controller] remove unused password validation

--- src/main/java/my/sport/application/service/PlayerService.java
+++ src/main/java/my/sport/application/service/PlayerService.java
@@ -4,58 +4,49 @@ import lombok.AllArgsConstructor;
 import my.sport.domain.entity.Player;
 import my.sport.domain.repository.PlayerRepository;
 import my.sport.domain.repository.RoleRepository;
-import my.sport.dto.UserDto;
 import my.sport.rest.dto.CreateUserCommandDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 
 @AllArgsConstructor
 public class PlayerService {
 
-	private PlayerRepository playerRepository;
-	private PasswordEncoder passwordEncoder;
-	private RoleRepository roleRepository;
+    private final PlayerRepository playerRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
-	@Transactional
-	public Player getPlayerById(Long id) {
-		return playerRepository.findById(id).orElse(null);
-	}
+    @Transactional
+    public Player getPlayerByEmail(String email) {
+        return playerRepository.findUserByEmail(email);
+    }
 
-	
-	@Transactional
-	public Player getPlayerByEmail(String email) {
-		return playerRepository.findUserByEmail(email);
-	}
 
-	
-	@Transactional
-	public Player add(CreateUserCommandDTO createUserCommandDTO) {
-		Player player = new Player();
-		player.setEmail(createUserCommandDTO.getEmail());
-		player.setFirstName(createUserCommandDTO.getFirstName());
-		player.setLastName(createUserCommandDTO.getLastName());
-		player.setPassword(passwordEncoder.encode(createUserCommandDTO.getPassword()));
-		player.setRoles(Arrays.asList(roleRepository.findByName("PLAYER")));
-		playerRepository.save(player);
-		return player;
-	}
-	
-	@Transactional
-	public void deletePlayer(String email) {
-		playerRepository.deleteByEmail(email);
-	}
+    @Transactional
+    public Player add(CreateUserCommandDTO createUserCommandDTO) {
+        Player player = new Player();
+        player.setEmail(createUserCommandDTO.getEmail());
+        player.setFirstName(createUserCommandDTO.getFirstName());
+        player.setLastName(createUserCommandDTO.getLastName());
+        player.setPassword(passwordEncoder.encode(createUserCommandDTO.getPassword()));
+        player.setRoles(Collections.singletonList(roleRepository.findByName("PLAYER")));
+        playerRepository.save(player);
+        return player;
+    }
 
-	
-	public Player getSessionPlayer() {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String email = auth.getName();
-		return playerRepository.findUserByEmail(email);
-	}
+    @Transactional
+    public void deletePlayer(String email) {
+        playerRepository.deleteByEmail(email);
+    }
+
+
+    public Player getSessionPlayer() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+        return playerRepository.findUserByEmail(email);
+    }
 }
--- src/main/java/my/sport/rest/dto/CreateUserCommandDTO.java
+++ src/main/java/my/sport/rest/dto/CreateUserCommandDTO.java
@@ -1,13 +1,11 @@
 package my.sport.rest.dto;
 
 import lombok.Data;
-import my.sport.domain.validator.PasswordMatches;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
-@PasswordMatches
 public class CreateUserCommandDTO {
 
     @NotBlank

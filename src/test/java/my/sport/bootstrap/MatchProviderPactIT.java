325387b2b439433aabf2846c17dc863574712d46
xuanchuongdp <xuanchuongdp@gmail.com>
2020-06-28 15:13:37 +0700

    [provider-pact-test] init pact test

--- src/test/java/my/sport/bootstrap/MatchProviderPactIT.java
+++ src/test/java/my/sport/bootstrap/MatchProviderPactIT.java
@@ -1,10 +1,18 @@
 package my.sport.bootstrap;
 
 import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import my.sport.MySportSpringApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = MySportSpringApplication.class)
 @PactFolder("pact")
@@ -12,4 +20,20 @@ import org.springframework.test.context.ActiveProfiles;
 @Provider("match-api")
 public class MatchProviderPactIT {
 
+    private static ConfigurableWebApplicationContext application;
+
+    @BeforeEach
+    void setUp(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", 8081, "/rest/api/v1/match"));
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void testTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+    @State("get all available matches")
+    public void testGetAllMatches() {
+    }
 }

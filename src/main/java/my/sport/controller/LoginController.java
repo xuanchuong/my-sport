cf20252ae3767cfa3fbd39ebd01740d6a5ebe877
xuanchuongdp <xuanchuongdp@gmail.com>
2020-09-28 22:05:03 +0700

    [#16] handle login and register

---
+++ src/main/java/my/sport/controller/LoginController.java
@@ -0,0 +1,22 @@
+package my.sport.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    @GetMapping
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping
+    public ModelAndView login(String username, String passwork, ModelMap model) {
+        return new ModelAndView("redirect:/dashboard", model);
+    }
+}

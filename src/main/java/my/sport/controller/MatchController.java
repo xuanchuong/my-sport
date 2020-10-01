0e31fa6a8a9957c5f0fc1531a144be4e52356f59
chuong.nguyen <chuong.nguyen@mobi.ch>
2020-10-01 12:04:14 +0700

    [#16] auto reload html change

---
+++ src/main/java/my/sport/controller/MatchController.java
@@ -0,0 +1,40 @@
+package my.sport.controller;
+
+import my.sport.application.service.FootballMatchService;
+import my.sport.domain.entity.FootballMatch;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/match")
+public class MatchController {
+
+	@Autowired
+	private FootballMatchService matchService;
+	@GetMapping
+	public String createMatch() {
+		return "createMatchForm";
+	}
+	
+	@GetMapping("/detail")
+	public String getMatchDetail(@RequestParam String id, Model model) {
+		FootballMatch footballMatch = matchService.getMatchById(Long.valueOf(id));
+		model.addAttribute("match", footballMatch);
+		return "matchDetail";
+	}
+	
+	@PostMapping("/join")
+	public String joinTheMatch( @RequestParam String id) {
+		return "dashboard";
+	}
+	
+	@PostMapping
+	public String creatingMatch() {
+		return "createMatchConfirm";
+	}
+}

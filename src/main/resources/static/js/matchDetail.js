9852d3fab006ff6b2962fd2a80f0f6d89fa3caf7
chuong.nguyen <chuong.nguyen@mobi.ch>
2020-10-16 17:32:31 +0700

    [#18] show confirm modal

--- src/main/resources/static/js/matchDetail.js
+++ src/main/resources/static/js/matchDetail.js
@@ -3,13 +3,14 @@ function reloadPage () {
 	window.location.reload();
 }
 
-$('button[name="joinBt"]').click(function (event) {
+$('button[name="confirmJoinBt"]').click(function (event) {
 	event.preventDefault();
+
 	const data = $('form').serialize();
 	$.post('/match/join', data, reloadPage);
 });
 
-$('button[name="leaveBt"]').click(function (event) {
+$('button[name="confirmLeaveBt"]').click(function (event) {
 	event.preventDefault();
 	const data = $('form').serialize();
 	$.post('/match/leave', data, reloadPage);

b0bf1f765c7a501c615567bb195566cbb010d4d2
xuanchuongdp <xuanchuongdp@gmail.com>
2020-10-18 14:32:08 +0700

    [#18] remove gitignore files

--- bootstrap/target/classes/migration/V1.2__create_match_players_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE match_players (
-    jpa_football_match_id BIGINT NOT NULL,
-    paticipants_id BIGINT NOT NULL
-)
--- domain/src/main/java/domain/vo/CreateUserCommand.java
+++ domain/src/main/java/domain/vo/CreateUserCommand.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
-import org.codehaus.plexus.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.function.BooleanSupplier;
 import java.util.stream.Stream;
